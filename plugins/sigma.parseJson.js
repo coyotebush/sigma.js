e2d70dda525f6360fcf583069e1e8b0069d56829
Corey Ford <coyotebush22@gmail.com>
2013-04-17 15:32:31 -0700

    clean up parseJson plugin

--- plugins/sigma.parseJson.js
+++ plugins/sigma.parseJson.js
@@ -1,23 +1,23 @@
 // Scott Hale (Oxford Internet Institute)
 // Requires sigma.js and jquery to be loaded
 // based on parseGexf from Mathieu Jacomy @ Sciences Po Médialab & WebAtlas
-sigma.publicPrototype.parseJson = function(jsonPath,callback) {
+
+sigma.publicPrototype.parseJson = function(jsonPath, callback) {
 	var sigmaInstance = this;
 	var edgeId = 0;
 	jQuery.getJSON(jsonPath, function(data) {
-		for (i=0; i<data.nodes.length; i++){
-			var id=data.nodes[i].id;
-			//window.NODE = data.nodes[i];//In the original, but not sure purpose
-			sigmaInstance.addNode(id,data.nodes[i]);
+		for (var i=0; i<data.nodes.length; i++){
+			var node=data.nodes[i];
+			sigmaInstance.addNode(node.id, node);
 		}
-		for(j=0; j<data.edges.length; j++){
-			var edgeNode = data.edges[j];
 
-			var source = edgeNode.source;
-			var target = edgeNode.target;
-
-			sigmaInstance.addEdge(edgeId++,source,target,edgeNode);
+		for (var i=0; i<data.edges.length; i++){
+			var edge=data.edges[i];
+			sigmaInstance.addEdge(edge.id, edge.source, edge.target, edge);
 		}
-		if (callback) callback.call(this);//Trigger the data ready function
-	});//end jquery getJSON function
-};//end sigma.parseJson function
+
+		if (callback) {
+			callback.call(this);
+		}
+	});
+};
